a9956253f1d9613753fa661e542f57c364453b3d
Harsh Thakur <73895659+iharsh02@users.noreply.github.com>
2024-03-27 22:18:26 +0530

    Refector : Remove Duplicate Style

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius="10px" overflow="hidden" width="300px">
+    <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading size="xl">{game.name}</Heading>
